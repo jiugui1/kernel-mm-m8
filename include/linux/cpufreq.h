db9008e90af7efd2d1833753ff21189dee05b254
Chet Kener <Cl3Kener@gmail.com>
2014-10-21 10:54:13 -0400

    cpufreq: Abyssplug: Add back all Governors
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    
    Original authorship is, quite frankly, uncertain to me.  This is what I see in the file
    
    Top of file:
     *  Copyright (C)  2001 Russell King
     *            (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
     *                      Jun Nakajima <jun.nakajima@intel.com>
     *            (C)  2012 Dennis Rassmann <showp1984@gmail.com>
    
    Bottom of file:
    MODULE_AUTHOR("Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>");
    MODULE_AUTHOR("Alexey Starikovskiy <alexey.y.starikovskiy@intel.com>");
    MODULE_DESCRIPTION("'cpufreq_abyssplugv2' - An abyssplug cpufreq governor based on ondemand");
    
    I can't be sure, but it looks like the original Abyssplug may have been written by the guys at intel
    but then rewritten into abyssplugv2 by Dennis Rassman.  Unfortunately, I've been unable to find a clean
    commit history in Dennis's repos. I'd love for someone to hook me up with som solid commit history on
    this.  I'd be more than happy to make the edits!  I surely didn't write it!
    
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -370,6 +370,15 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
+extern struct cpufreq_governor cpufreq_gov_uberdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_uberdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ADAPTIVE)
+extern struct cpufreq_governor cpufreq_gov_adaptive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_adaptive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
+extern struct cpufreq_governor cpufreq_gov_brazilianwax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
@@ -397,6 +406,12 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HELLSACTIVE)
 extern struct cpufreq_governor cpufreq_gov_hellsactive;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_hellsactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUG)
+extern struct cpufreq_governor cpufreq_gov_abyssplug;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_abyssplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ABYSSPLUGV2)
+extern struct cpufreq_governor cpufreq_gov_abyssplugv2;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_abyssplugv2)
 #endif
 
 
